b692aa206961f84a01e28c4dbf21d85627d2c0e3
Paulalemany <palemany@ucm.es>
2026-02-19 19:05:21 +0100

    Esqueleto muy muy básico del level manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    //Controla todo lo que sucede en un nivel
+    #region Referencias
+
+    [SerializeField]
+    GameObject Teatro;  //Todo lo referente a la parte del teatro
+
+    [SerializeField]
+    GameObject Investigacion;   //Todo lo referente a la parte de moverse e interactuar
+    GameObject player;
+
+    [SerializeField]
+    GameObject Resolucion;
+
+    #endregion
+
+    #region Propiedades
+
+    bool objetivo;  //Booleano que marca si se ha cumplido o no el objetivo
+
+    #endregion
+
+    #region Metodos
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        objetivo = false;
+    }
+
+    #endregion
+}
