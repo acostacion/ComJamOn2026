5af4604eb5ba41f6b966a2552ec58ba6ba2b02fb
M00nHun7r3ss <116196853+M00nHun7r3ss@users.noreply.github.com>
2026-02-22 18:54:29 +0100

    Animacion de la brujita

--- Assets/Scripts/2. Action/Movement.cs
+++ Assets/Scripts/2. Action/Movement.cs
@@ -4,46 +4,45 @@ using UnityEngine.InputSystem;
 public class Movement : MonoBehaviour
 {
     CharacterController characterController;
-    Transform myTransform;
     SpriteRenderer sprite;
+    Animator animator;
+
     float flip;
-    Vector2 moveInput;
+    public Vector2 moveInput;
 
     private float posZ;
     private float posY;
 
-    [SerializeField]
-    private float vel = 5;
+    [SerializeField] private float vel = 5;
+    [SerializeField] private bool canFlip;
 
-    [SerializeField]
-    private bool canFlip;
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         characterController = GetComponent<CharacterController>();
         sprite = GetComponent<SpriteRenderer>();
-        myTransform = GetComponent<Transform>();
-        flip = 1;   //Siempre comenzamos mirando a la derecha
+        animator = GetComponent<Animator>();
 
-        posZ = myTransform.position.z;
-        posY = myTransform.position.y;
+        flip = 1;
+
+        posZ = transform.position.z;
+        posY = transform.position.y;
     }
 
-    // Update is called once per frame
     void Update()
     {
         characterController.Move(new Vector3(moveInput.x * Time.deltaTime * vel, 0, 0));
 
         transform.position = new Vector3(transform.position.x, posY, posZ);
 
-        //Si cambiamos de dirección volteamos el sprite
+        // Animacion
+        animator.SetFloat("Speed", Mathf.Abs(moveInput.x));
+
+        // Flip sprite
         if (canFlip && moveInput.x != flip && moveInput.x != 0)
         {
             sprite.flipX = !sprite.flipX;
             flip = moveInput.x;
         }
-            
     }
 
     public void OnMove(InputAction.CallbackContext context)
